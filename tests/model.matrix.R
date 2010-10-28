d19328db632e5c987ce68fa4c28c67610bacec44
Therneau Terry <terry.therneau@mayo.edu>
2010-10-27 20:26:39 -0500

    Added more comments.

--- tests/model.matrix.R
+++ tests/model.matrix.R
@@ -14,6 +14,7 @@ fit1 <- coxph(Surv(time, status) ~ z, test1, iter=1)
 fit2 <- coxph(Surv(time, status) ~z, test1, x=T, iter=1)
 all.equal(model.matrix(fit1), fit2$x)
 
+# This has no level 'b', make sure dummies recode properly
 test2 <- data.frame(time=  c(9, 3,1,1,6,6,8),
                     status=c(1,NA,1,0,1,1,0),
                     x=     c(0, 2,1,1,1,0,0),
@@ -22,7 +23,9 @@ test2 <- data.frame(time=  c(9, 3,1,1,6,6,8),
 ftest <- model.frame(fit1, data=test2)
 all.equal(levels(ftest$z), levels(test1$z))
 
+# xtest will have one more row than the others, since it does not delete
+#  the observation with a missing value for status
 xtest <- model.matrix(fit1, data=test2)
 dummy <- fit2$x
 dummy[,1] <- 0
-all.equal(xtest, dummy)
+all.equal(xtest[-2,], dummy, check.attributes=FALSE)
