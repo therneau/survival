1040c24ce5dea35de80ce0d0ac3b75bd1fd60cf3
Terry Therneau <therneau@mayo.edu>
2011-04-13 12:49:14 -0500

    Remove "INDEX" file, now terribly outdated.  Revert the change in coxph that set y=!model to the old y=TRUE default.  Add a test for -1 in a coxph formula.

--- tests/doaml.R
+++ tests/doaml.R
@@ -53,4 +53,11 @@ x1
 x2 <- coxph(Surv(rep(0,23),time, status) ~x, aml, method='efron')
 aeq(x1$coef, x2$coef)
 
+# Test the drop of an itercept: should have no effect
+fit2 <- coxph(Surv(Surv(time, status) ~ x -1, method='breslow',
+                   data=aml)
+aeq(fit$loglik, fit2$loglik)
+aeq(coef(fit), coef(fit2))
+aeq(fit$var, fit2$var)
+
 rm(x1, x2, atime, denom, deaths, chaz,cvar, tfit, sfit, temp, temp2, fit)
