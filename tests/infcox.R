b50bc7a183a3a1f109a96045a0ed706817085656
Therneau Terry <terry.therneau@mayo.edu>
2012-01-31 21:48:21 -0600

    1. Make sure that "linear predictor" predictions are a vector and not a matrix. 2. Fix an error message in survdiff. 3. Reconcile some test results (minor issues), and documentation.

--- tests/infcox.R
+++ tests/infcox.R
@@ -7,4 +7,11 @@ library(survival)
 #
 test3 <- data.frame(futime=1:12, fustat=c(1,0,1,0,1,0,0,0,0,0,0,0),
 		   x1=rep(0:1,6), x2=c(rep(0,6), rep(1,6)))
-coxph(Surv(futime, fustat) ~ x1 + x2, test3, iter=25)
+
+# This will produce a warning message, which is the point of the test.
+# The variance is close to singular and gives different answers 
+#  on different machines
+fit3 <- coxph(Surv(futime, fustat) ~ x1 + x2, test3, iter=25)
+
+all.equal(round(fit3$coef,1), c(-22.2, -22.9), check.attributes=FALSE)
+all.equal(round(fit3$log, 4),c(-6.8669, -1.7918))
