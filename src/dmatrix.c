7918d406697d51b26a0b5c9f6862040c079f1dba
Terry Therneau <terry.therneau@gmail.com>
2012-12-04 21:03:32 -0600

    Add C code for multi-state.

--- src/dmatrix.c
+++ src/dmatrix.c
@@ -1,15 +1,17 @@
 /* $Id: dmatrix.c 11357 2009-09-04 15:22:46Z therneau $
 **
-** set up ragged arrays, with #of columns and #of rows
+** set up ragged arrays, with #of columns and #of rows,
+**  where nrow (second arg) is what R thinks are columns
+**  but C thinks are rows.
 */
 #include "survS.h"
 #include "survproto.h"
 
 double **dmatrix(double *array, int ncol, int nrow)
     {
-S_EVALUATOR
-    register int i;
-    register double **pointer;
+
+    int i;
+    double **pointer;
 
     pointer = (double **) ALLOC(nrow, sizeof(double *));
     for (i=0; i<nrow; i++) {
