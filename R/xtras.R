4e5f6a140b49d980176d470dc4da28b5dbc292a8
Terry Therneau <terry.therneau@gmail.com>
2013-05-03 17:03:42 -0500

    Add a rep() method for Surv objects

--- R/xtras.R
+++ R/xtras.R
@@ -1,20 +1,24 @@
-# The vcov and extractAIC methods are not defined in Splus, so they
-#  do not need survival methods.
-if (is.R()) {
-   vcov.coxph<-function (object, ...) {
+vcov.coxph<-function (object, ...) {
         rval<-object$var
         dimnames(rval)<-list(names(coef(object)),names(coef(object)))
         rval
     }
 
-    vcov.survreg<-function (object, ...) {
+vcov.survreg<-function (object, ...) {
         object$var
     }
 
 
-    extractAIC.coxph.penal<- function(fit,scale,k=2,...){
+extractAIC.coxph.penal<- function(fit,scale,k=2,...){
         edf<-sum(fit$df)
         loglik <- fit$loglik[length(fit$loglik)]
         c(edf, -2 * loglik + k * edf)
     }
+
+
+labels.survreg <- function(object, ...) attr(object,"term.labels")
+
+rep.Surv <- function(x, ...) {
+    indx <- rep(1:nrow(x), ...)
+    x[indx,]
 }
