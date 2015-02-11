514dc0df0ebfae6b2c57132b5e80aeba6f21f6cf
Terry Therneau <terry.therneau@gmail.com>
2015-02-11 12:51:56 -0600

    Revert changes to basehaz, other small bugs in the testing phase.

--- R/basehaz.R
+++ R/basehaz.R
@@ -6,13 +6,20 @@ basehaz <- function (fit, centered = TRUE)
 {
     if (!inherits(fit, "coxph")) 
         stop("must be a coxph object")
-    if (!centered) {
-        tdata <- as.list(0*fit$means)  #dummy data set of all zeros
-        sfit <- survfit(fit, newdata=tdata, se.fit=FALSE)
-    }
-    else sfit <- survfit(fit, se.fit=FALSE)
+    sfit <- survfit(fit, se.fit=FALSE)   
 
-    new <- data.frame(hazard=sfit$cumhaz, time=sfit$time)
+    if (!centered) {
+        # The right thing to do here is to call survfit with a vector of
+        #  all zeros for the "subject to predict".  But if there is a factor
+        #  in the model, there may be no subject at all who will give all
+        #  zeros, so we post process instead
+        zcoef <- ifelse(is.na(coef(fit)), 0, coef(fit))
+        offset <- sum(fit$means * zcoef)
+        chaz <- sfit$cumhaz * exp(-offset)
+    }
+    else chaz <- sfit$cumhaz
+
+    new <- data.frame(hazard=chaz, time=sfit$time)
 
     strata <- sfit$strata
     if (!is.null(strata)) 
--- tests/survfit2.R
+++ tests/survfit2.R
@@ -11,3 +11,7 @@ fit2 <- survfit(Surv(time, status) ~1, tdata)
 stdlow <- fit2$std * sqrt(c(1, 10/9, 1, 8/7, 1, 6/5, 6/4, 6/3, 1, 2/1))
 lower <- exp(log(fit2$surv) - qnorm(.975)*stdlow)
 all.equal(fit1$lower, lower, check.attributes=FALSE)
+
+
+
+# Test out leverage residuals
