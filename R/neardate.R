ee8dd94458bd3cd820dd6530d3cdd857a5e993f4
Terry Therneau <terry.therneau@gmail.com>
2015-09-16 17:30:08 -0500

    Fix an issue with unsorted inputs.

--- R/neardate.R
+++ R/neardate.R
@@ -67,9 +67,9 @@ neardate <- function(id1, id2, y1, y2, best=c("after", "prior"),
 
     if (best=="prior")
         indx2 <- approx(hash2, 1:n2, hash1, method="constant", yleft=NA,
-                        yright=n2, rule=2, f=0)$y
+                        rule=2, f=0)$y
     else 
-        indx2 <- approx(hash2, 1:n2, hash1, method="constant", yleft=1,
+        indx2 <- approx(hash2, 1:n2, hash1, method="constant",
                         yright=NA, rule=2, f=1)$y
 
     rowid[ifelse(id1== id2[indx2], indx2, NA)]
