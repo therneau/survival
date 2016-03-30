6253e367a3e0e5bf9681c5263baf56105aa77d04
Terry Therneau <terry.therneau@gmail.com>
2016-03-30 18:06:51 -0500

    Finish the finegray function, now to test it.

--- src/finegray.c
+++ src/finegray.c
@@ -1,50 +1,46 @@
 /*
 ** Do the indexing for a Fine-Gray model
-**  this routine has a lot of overlap with survsplit
 ** input: (tstart, tstop] the time interval for each obs
-**        cut: the vector of new cutpoints
-**        prob: survival estimates for any new intervals
-**        extend: should this observation be extended?
 **        ctime, cprob: the underlying survival curve
+**        the interval that ends at ctime has probability cprob
+**        extend: should this observation be extended?
+**        keep: output the interval from ctime[i] to ctime[i+1]?
 **
 ** output: row = which row of the original data, for each row of the new
 **         (start, end] = the new time intervals
-**         wt           = probability weight for the interval.  This is 1.0
+**          wt          = probability weight for the interval.  This is 1.0
 **             for all but the extended intervals.
 */
 #include "survS.h"
 #include "survproto.h"
 #include <stdio.h>
 
-SEXP finegray(SEXP tstart2, SEXP tstop2,   SEXP cut2,   SEXP prob2, 
-	      SEXP extend2, SEXP ctime2, SEXP cprob2) {
-    int i,j, k, extra;
+SEXP finegray(SEXP tstart2, SEXP tstop2,   SEXP ctime2,   SEXP cprob2, 
+	      SEXP extend2, SEXP keep2) {
+    int i,j, k, iadd, extra;
     int n;   /* number of observations */
-    int ncut; /* number of cuts */
+    int ncut; /* number of censoring intervals */
     int n2;   /* number of new obs */
-    int nprob;
     double tempwt;
 
-    double *tstart, *tstop, *cut;
-    double *ctime,  *cprob, *prob;
-    int *extend;
+    double *tstart, *tstop;
+    double *ctime,  *cprob;
+    int *extend, *keep;
 
     /* returned objects */
-    SEXP row2, start2, end2, wt2, rlist;
+    SEXP row2, start2, end2, wt2, add2, rlist;
     double *start, *end, *wt;
-    int *row;
-    static const char *outnames[]={"row", "start", "end", "wt", ""};
+    int *row, *add;
+    static const char *outnames[]={"row", "start", "end", "wt", "add",  ""};
 
     n = LENGTH(tstart2);
-    ncut = LENGTH(cut2);
+    ncut = LENGTH(cprob2);
     tstart = REAL(tstart2);
     tstop  = REAL(tstop2);
-    cut   = REAL(cut2);
-    prob  = REAL(prob2);
-    extend= INTEGER(extend2);
-    nprob  = LENGTH(ctime2);
-    ctime  = REAL(ctime2);
-    cprob  = REAL(cprob2);
+    extend= LOGICAL(extend2);
+    keep  = LOGICAL(keep2);
+    ctime = REAL(ctime2);
+    cprob = REAL(cprob2);
 
     /* 
     ** how many obs will I need? NA inputs are left alone.
@@ -54,11 +50,16 @@ SEXP finegray(SEXP tstart2, SEXP tstop2,   SEXP cut2,   SEXP prob2,
     extra =0;
     for (i=0; i<n; i++) {
 	if (!ISNAN(tstart[i]) && !ISNAN(tstop[i]) && extend[i] ) {
-	    for (j=0; (j<ncut) && (cut[j] <= tstop[i]); j++);
-	    if (j < ncut) extra += (ncut -j);
+	    for (j=0; (j<ncut) && (ctime[j] < tstop[i]); j++);
+	    /* j = the interval they lie in, 0 = before the first cut
+	       point, 1 = within the first interval.  We have to add 
+	       any intervals after the one the subject falls in;
+	    */
+	    j++;
+	    for (; j<ncut; j++) extra += keep[j];
 	}
     }	
-    
+
     /* allocate output */
     n2 = n + extra;
     PROTECT(rlist = mkNamed(VECSXP, outnames));
@@ -71,6 +72,8 @@ SEXP finegray(SEXP tstart2, SEXP tstop2,   SEXP cut2,   SEXP prob2,
     end = REAL(end2);
     wt2 =    SET_VECTOR_ELT(rlist, 3, allocVector(REALSXP, n2));
     wt = REAL(wt2);
+    add2 =   SET_VECTOR_ELT(rlist, 4, allocVector(INTSXP, n2));
+    add = integer(add2);
     
     /* do the work */
     k =0; 
@@ -80,44 +83,29 @@ SEXP finegray(SEXP tstart2, SEXP tstop2,   SEXP cut2,   SEXP prob2,
 	end[k]   = tstop[i];
 	row[k]  = i+1;    /* 1 based subscripts for R */
 	wt[k] =1;
+	add[k] =0;
     
-	if (extend[i] && cut[ncut-1] > tstop[i]) {
-	    /* 
-	    ** Add more itervals at the end 
-	    **  tempwt = weight for the interval in which they ended
-	    **  If they are removed at exactly a cutpoint, the assumption
-	    **  is that censoring was not possible on that given day.
-	    */
-	    tempwt = 1;
-	    for (j=0; (j<nprob) && (ctime[j] < tstop[i]); j++) {
-		tempwt = cprob[j];
-	    }		
-	    printf("i= %d, stop=%f, tempwt=%f\n", i, tstop[i], tempwt);
+	if (!ISNAN(tstart[i]) && !ISNAN(tstop[i]) && extend[i]) {
+	    /* ctime contains the time at the end of the interval */
+	    for (j=0; (j<ncut) && (ctime[j] < tstop[i]); j++);
+	    end[k] = ctime[j];  /* extend them to the end of the interval */
+	    tempwt = cprob[j];
 
-	    for (j=0; (j<ncut) && (cut[j] <= tstop[i]); j++);
-	    printf(" j=%d, cut=%f, stop=%f\n", j, cut[j], tstop[i]);
-	    if (prob[j] == tempwt) {
-		/* tstop < cut, but the censoring distribution has no
-		   jumps before that cut
-		*/
-		end[k] = cut[j];
-		j++;
-		}
-	    if (j < ncut) {
-		k++;
-		row[k] = i+1;
-		start[k] = tstop[i];
-		end[k]  = cut[j];
-		wt[k]   = prob[j]/tempwt;
-		for (j=j+1; j<ncut; j++) {
+	    j++;
+	    iadd=0;
+	    for (; j < ncut; j++) {
+		if (keep[j]) {
+		    /* add more */
 		    k++;
+		    iadd++;
 		    row[k] = i+1;
-		    start[k] = cut[j-1];
-		    end[k] = cut[j];
-		    wt[k] = prob[j]/tempwt;
+		    start[k] = ctime[j-1];
+		    end[k]  = ctime[j];
+		    wt[k]   = cprob[j]/tempwt;
+		    add[k]  = iadd;
 		}
 	    }
-	}
+	}	
 	k++;
     }
     UNPROTECT(1);
--- tests/finegray.R
+++ tests/finegray.R
@@ -1,24 +1,24 @@
 library(survival)
 
 # Test data set for Fine-Gray regression
-fdata <- data.frame(time  =c(1,2,3,4,4,5,5,5,5,6,8,9,10,12),
-                    status=c(1,2,0,1,0,2,0,0,1,0,2,0,1 ,0),
+fdata <- data.frame(time  =c(1,2,3,4,4,4,5,5,6,8,8, 9,10,12),
+                    status=factor(c(1,2,0,1,0,0,2,1,0,0,2, 0,1 ,0), 0:2,
+                             c("cen", "type1", "type2")),      
                     x     =c(5,4,3,1,2,1,1,2,2,4,6,1,2, 0),
                     id = 1:14)
-fstat <- factor(fdata$status, 0:2, c("cen", "type1", "type2"))
-test1 <- finegray(Surv(time, fstat) ~x, fdata)
-test2 <- finegray(Surv(time, fstat) ~x, fdata, event=2)
+test1 <- finegray(Surv(time, status) ~., fdata)
+test2 <- finegray(Surv(time, status) ~x, fdata, event=2)
 
 # When creating the censoring time distribution remember that
 #  censors happen after deaths, so the distribution does not drop until
-#  time 3+, 4+, 5+, 6+ and 9+
-csurv <- list(time=c(0, 3, 4, 5, 6, 9),
-              p = cumprod(c(1, 11/12, 9/10, 5/7, 4/5, 2/3)))
+#  time 3+, 4+, 6+, 8+ and 9+
+csurv <- list(time=c(0, 3, 4, 6, 8, 9),
+              p = cumprod(c(1, 11/12, 8/10, 5/6, 3/4, 2/3)))
 #
 # For estimation of event type 1, the first subject of event type
-#  2 will have weights of curve$p over (0,3], (3,4], (4,5], (5,6], (6,9] 
+#  2 will have weights of curve$p over (0,3], (3,4], (4,6], (6,8], (8,9] 
 #  and (9,12].  All that really matters is the weight at times 1, 4, 5,
-#  and 10, however, which are the points at which events of type 1 happen.
+#  and 10, however, which are the points at which events of type 1 happen
 #  
 # The next subject of event type 2 occurs at time 5, and will have a
 #  weight of (9,12] /(4,5] = (5*4*2)/(7*5*3) = 8/21 at time 10.  The last
@@ -34,7 +34,8 @@ row.names(temp1) <- NULL
 tfit <- survfit(Surv(start, time, status==1) ~1, temp1, weight=wt)
 
 
-ctest <- crprep("time", "status", data=fdata, trans=1:2, cens=0, 
+fdata$stat2 <- as.numeric(fdata$status) -1
+ctest <- crprep("time", "stat2", data=fdata, trans=1:2, cens=0, 
                 keep=c("id", 'x'))
 subset(ctest, failcode==1)
 
