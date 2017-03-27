f3ac93704949ff26e07720b56f2b18ffa8066470
Terry Therneau <terry.therneau@gmail.com>
2017-03-27 10:46:46 -0500

    Remove the exection permission flag from .rda files.  Add survexp.rda to tracking.  Update inst.c to notice the R version.

--- src/init.c
+++ src/init.c
@@ -6,6 +6,7 @@
 */
 #include "survS.h"
 #include "R_ext/Rdynload.h"
+#include "Rversion.h"
 #include "survproto.h"
 
 static const R_CMethodDef Centries[] = {
@@ -65,7 +66,10 @@ void R_init_survival(DllInfo *dll){
     /*
     ** This line makes them only be available via the symbols above
     **  i.e., .Call("tmerge", ) won't work but .Call(Ctmerge, )  will
+    ** This feature was added in version 2.16
     */
+if defined(R_VERSION) && R_VERSION >= R_Version(2, 16, 0)
     R_forceSymbols(dll, TRUE);
+#endif
 }
     
