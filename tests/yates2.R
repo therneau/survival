7b5303e64b6dc8efa10117410157c08be6d6e062
therneau <none@none>
2018-01-01 21:07:19 -0600

    Testing a tuning of yates function.

--- tests/yates2.R
+++ tests/yates2.R
@@ -1,4 +1,6 @@
 library(survival)
 # Tests for glm
 
-gfit1 <- glm(skips ~ Mask + Opening, data=solder, poisson)
+gfit1 <- glm(skips ~ Mask* Opening + Solder, data=solder, poisson)
+yg1 <- yates(gfit1, ~Mask)
+yg2 <- yates(gfit1, ~ Mask, type='response')
