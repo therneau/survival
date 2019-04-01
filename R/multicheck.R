aae6db3f39c7f21cda3d8a2414d7a02438d23433
Therneau Terry <terry.therneau@mayo.edu>
2019-04-01 06:00:25 -0500

    Update the multicheck routine to add a user interface

--- R/multicheck.R
+++ R/multicheck.R
@@ -1,16 +1,58 @@
+multicheck <- function(formula, data, id, istate, ...) {
+    Call <- match.call()
+    indx <- match(c("formula", "data", "id", "istate"),
+                  names(Call), nomatch=0) 
+    if (indx[1] ==0) stop("A formula argument is required")
+    tform <- Call[c(1,indx)]  # only keep the arguments we wanted
+    tform[[1L]] <- quote(stats::model.frame)  # change the function called
+
+    mf <- eval(tform, parent.frame())
+    
+    Y <- model.response(mf)
+    if (!inherits(Y, "Surv")) stop("response must be a survival object")
+    type <- attr(Y, "type")
+    if (!(type %in% c("mright", "mcounting")))
+        stop("response must be a multi-state survival object")
+    n <- nrow(Y)
+    
+    id <- model.extract(mf, "id")
+    if (is.null(id)) stop("an id argument is required")
+    else if (length(id) !=n) stop("wrong length for id")
+    
+    istate <- model.extract(mf, "istate")
+    if (!is.null(istate) && length(istate) !=n) stop("wrong length for istate")
+
+    fit <- multicheck2(Y, id, istate)
+    fit$istate <- NULL
+    na.action <- attr(mf, "na.action")
+    if (!is.null(na.action)) {
+        # make any numbering match the input data, not the retained data
+        lost <- unclass(na.action)  # the observations that were removed
+        dummy <- seq.int(1, n+ length(lost))[-lost]
+        for (i in c("overlap", "gap", "teleport", "jump")){
+            if (!is.null(fit[[i]])$row) {
+                temp <- fit[[i]]
+                temp$row <- dummy[temp$row]
+                fit[[i]] <- temp
+            }
+        }
+    }
+    fit
+}
+
 #
 # The multi-state routines need to check their input data
-#  y = 3 column survival object
+#  y = survival object
 #  id = subject identifier
 #  istate = starting state for each row, this can be missing.
 # 
-multicheck <- function(y, id, istate=NULL, dummy="( )") {
+multicheck2 <- function(y, id, istate=NULL, dummy="( )") {
     n <- length(id)
-    # this next line is a debug for me, since multicheck is not visible
+    # this next line is a debug for my code, since multicheck2 is not visible
     #  to users
     if (!is.Surv(y) || is.null(attr(y, "states")) ||
         any(y[,ncol(y)] > length(attr(y, "states"))))
-        stop("multicheck called with an invalid y argument")
+        stop("multicheck2 called with an invalid y argument")
     to.names <- c("(censored)", attr(y, "states"))
  
     if (length(istate)==0) {
@@ -23,15 +65,20 @@ multicheck <- function(y, id, istate=NULL, dummy="( )") {
         inull <- FALSE
     }
 
-    flags <- c(overlap=0, gaps=0, inconsistent=0, jumps=0)
+    # normalize the state names
+    states <- unique(c(levels(istate), attr(y, "states")))
+
+    # initialize counts
+    flag <- c(overlap=0, gap=0, teleport=0, jump=0)
+
     # Check 0: if y has only 2 colums there isn't much to do
     if (ncol(y)==2) {
-        flags["overlap"] <- sum(duplicated(id))
+        flag["overlap"] <- sum(duplicated(id))
         overlap <- list(row=duplicated(id), id=unique(id[duplicated(id)]))
         transitions <- table(istate, y[,2])
         dimnames(transitions)[[2]] <- to.names
         rval <- list(istate=istate, transitions=transitions,
-                    states=c(levels(istate), to.names[-1]), flags=flags)
+                    states=states, flag=flag)
         if (length(overlap)) rval$overlap <- overlap
         return(rval)
     }
@@ -44,23 +91,23 @@ multicheck <- function(y, id, istate=NULL, dummy="( )") {
     gap <- sign(y[indx1,1] - y[indx2,2])
     oldid <- duplicated(id[index])[-1]
     
-    # If no one has more than one obs our work is done: overlap and gaps are
+    # If no one has more than one obs our work is done: overlap and gap are
     #  impossible
     to.names <- c("", attr(y, "states"))
     if (all(!oldid)) {
         transitions <- table(istate, y[,2])
         dimnames(transitions)[[2]] <- to.names
         return(list(istate=istate, transitions=transitions,
-                    states=c(levels(istate), to.names[-1]), flags=flags))
+                    states=states, flag=flag))
     }
 
     # gap = 0   (0, 10], (10, 15]
     # gap = 1   (0, 10], (12, 15]  # a hole in the time
     # gap = -1  (0, 10], (9, 15]   # overlapping times
-    overlap <- which(gap <0) & oldid
+    overlap <- which((gap <0) & oldid)
     if (length(overlap)) {
-        flags["overlap"] <- length(overlap)
-        overlap <- list(id= (id[indx2])[overlap],
+        flag["overlap"] <- length(overlap)
+        overlap <- list(id= unique((id[indx2])[overlap]),
                         row = indx2[overlap])
     } 
 
@@ -74,38 +121,37 @@ multicheck <- function(y, id, istate=NULL, dummy="( )") {
     #  istate vector is NULL) and marches forward.
     id.int <- match(id, unique(id))
     if (inull) { # construct an istate
-        gaps <- which(oldid & gap==1)
-        if (length(gaps)) {
-            # data has gaps but no istate  
-            error["gaps"] <- length(gaps) # data has gaps but no istate
-            gaps <- list(id= (id[indx2])[gaps],
-                        row = indx2[gaps])
+        gap <- which(oldid & gap==1)
+        if (length(gap)) {
+            # data has gap but no istate  
+            error["gap"] <- length(gap) 
+            gap <- list(id= unique((id[indx2])[gap]),
+                        row = indx2[gap])
         }
         pstate <- .Call(Cmulticheck, y, id.int, integer(n), index-1L) 
         istate <- factor(pstate, 0:max(pstate), c("", to.names[-1])) 
         states <- c("", to.names[-1])
-        jumps <- NULL
+        jump <- NULL
     }
     else {
         pstate <- .Call(Cmulticheck, y, id.int, as.integer(istate), index-1L)
         # the result is a mixed factor, give it proper levels
         itemp <- length(levels(istate))
         ptemp <- ifelse(c(FALSE, oldid), pstate, pstate + itemp)
-        states <- unique(c(levels(istate), to.names[-1]))
         pstate <- factor(pstate, seq_len(itemp + length(to.names) -1), states)
                                          
         bad <- ((pstate != istate)[-1] & gap==0 & oldid)
         if (any(bad)) {
             temp <- which(bad)
-            flags["inconsistent"] <- length(temp)
-            inconsistent <- list(id=  (id[index])[temp], row= index[temp])
+            flag["teleport"] <- length(temp)
+            teleport <- list(id= unique((id[index])[temp]), row= index[temp])
         }
-        # jumps are unobserved transitions.  A subject changed to state 1 at 
+        # jump are unobserved transitions.  A subject changed to state 1 at 
         #  time 10, next obs is at time 20 in state 2 for instance.  
-        jumps <- which((pstate != istate)[-1] & gap==1 & oldid)
-        if (length(jumps)) {
-            flags["jumps"] <- length(jumps)
-            jumps <- list(id = (id[index])[jumps], row= index[jumps])
+        jump <- which((pstate != istate)[-1] & gap==1 & oldid)
+        if (length(jump)) {
+            flag["jump"] <- length(jump)
+            jump <- list(id = unique((id[index])[jump]), row= index[jump])
         }       
     }   
                            
@@ -114,11 +160,11 @@ multicheck <- function(y, id, istate=NULL, dummy="( )") {
     dimnames(trtable) <- list(from = levels(istate), to= to.names)   
     
     rval <- list(istate=istate, transitions =trtable, states=states, 
-                 flags =flags)
+                 flag =flag)
     if (length(overlap)) rval$overlap <- overlap
-    if (lengh(gaps))     rval$gaps <- gaps
-    if (any(bad))     rval$inconsistent <- inconsistent
-    if (length(jumps)) rval$jumps <- table(pstate[jumps], istate[jumps])
+    if (lengh(gap))     rval$gap <- gap
+    if (any(bad))     rval$teleport <- teleport
+    if (length(jump)) rval$jump <- table(pstate[jump], istate[jump])
     rval
 }
     
