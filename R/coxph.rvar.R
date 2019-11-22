3a7fd2d2e0068df5ee46d38de35dc33f353ec9d8
Terry Therneau <terry.therneau@gmail.com>
2019-11-22 06:58:50 -0600

    Updates to survfit to return IJ only at select times.

--- R/coxph.rvar.R
+++ R/coxph.rvar.R
@@ -1,7 +1,6 @@
-# $Id: coxph.rvar.S 11166 2008-11-24 22:10:34Z therneau $
 coxph.rvar <- function(fit, collapse) {
     rcall <- match.call()
-    if (class(fit) != 'coxph')
+    if (!inherits(fit, "coxph"))
 	stop ("First argument must be a fitted Cox model")
 
     if (missing(collapse)) temp <- residuals.coxph(fit, type='dfbeta')
