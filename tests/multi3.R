620f12ba00957d97beb44d2afa27be255b3ce607
Terry Therneau <terry.therneau@gmail.com>
2022-03-04 08:55:25 -0600

    Add a new test for multistate

--- tests/multi3.R
+++ tests/multi3.R
@@ -11,14 +11,13 @@ ptemp <- subset(pbc1, !duplicated(id))  # first row of each
 
 pbc2 <- tmerge(ptemp[, c("id", "age", "sex")], ptemp, id,
                death= event(futime, status==2))
-# the as.numeric works around a current bug in tmerge
+
 pbc2 <- tmerge(pbc2, pbc1, id=id, bili = tdc(day, bili),
                  bili4 = tdc(day, bili4), bstat = event(day, as.numeric(bili4)))
 btemp <- with(pbc2, ifelse(death, 5, bstat))
 
-# a row with the same starting and ending bili is not an event
-b2 <- ifelse(((as.numeric(pbc2$bili4)) == btemp), 0, 
-                btemp)
+# a row with the same starting and ending bili4 level is not an event
+b2 <- ifelse(((as.numeric(pbc2$bili4)) == btemp), 0, btemp)
 pbc2$bstat <- factor(b2, 0:5,
                      c("censor", "normal", "1-2x", "2-4x", ">4", "death"))
 check1 <- survcheck(Surv(tstart, tstop, bstat) ~ 1, istate= bili4,
@@ -28,7 +27,7 @@ all.equal(as.character(pbc2$bili4), as.character(check1$istate))
 
 # Time dependent fit
 fit1 <- coxph(Surv(tstart, tstop, death) ~ age + bili4, pbc2)
-# Force an additive multi-state fit, where bili4 is a state
+# An additive multi-state fit, where bili4 is a state
 #  The three forms below should all give identical models
 fit2 <- coxph(list(Surv(tstart, tstop, bstat) ~ 1,
                    c(1:4):5 ~ age / common + shared), id= id, istate=bili4,
@@ -46,3 +45,25 @@ aeq(names(coef(fit2)), c("age", "ph(2:5/1:5)", "ph(3:5/1:5)", "ph(4:5/1:5)"))
 all.equal(coef(fit2), coef(fit2b))
 all.equal(coef(fit2), coef(fit2c))
 
+# Now a model with a separate age effect for each bilirubin group
+fit3  <- coxph(Surv(tstart, tstop, death) ~ age*bili4, pbc2)
+fit3b <- coxph(Surv(tstart, tstop, death) ~ bili4/age, pbc2)
+fit4 <-  coxph(list(Surv(tstart, tstop, bstat) ~ 1,
+                   c(1:4):5 ~ age / shared), id= id, istate=bili4,
+              data=pbc2)
+
+# unpack the interaction, first 4 coefs will be the age effect within each
+#  bilirubin group
+temp <- c(coef(fit3)[1] + c(0, coef(fit3)[5:7]), coef(fit3)[2:4])
+names(temp)[1:4] <- c("age1", "age2", "age3", "age4")
+aeq(temp, coef(fit3b)[c(4:7, 1:3)])
+aeq(temp, coef(fit4))
+
+# Third, a model with separate baseline hazards
+fit5 <- coxph(Surv(tstart, tstop, death) ~ strata(bili4)/age, pbc2)
+fit6 <- coxph(list(Surv(tstart, tstop, bstat) ~ 1, 0:5 ~ age),
+                   id=id, istate=bili4, pbc2)
+aeq(coef(fit5), coef(fit6))
+
+# Now for the hard part; verify that the resulting estimated survival curves
+#  are correct
