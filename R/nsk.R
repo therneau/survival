c6bbe1527c6e2e435069a38173fec619e1cf5baf
Terry Therneau <terry.therneau@gmail.com>
2022-04-22 06:55:00 -0500

    Fix to nsk with Boundary.knots=FALSE

--- R/nsk.R
+++ R/nsk.R
@@ -4,14 +4,22 @@
 nsk <- function(x, df=NULL, knots=NULL, intercept=FALSE, b=.05,
                 Boundary.knots = quantile(x, c(b, 1-b), na.rm=TRUE)) {
 
-    if (is.logical(Boundary.knots) || length(Boundary.knots) == 0)  
-        kx <- range(x, na.rm=TRUE)   # the ns() default
-    else if (length(knots) == 0) kx <- Boundary.knots
-    else {
-        if (Boundary.knots[2] <= max(knots)) Boundary.knots <- Boundary.knots[1]
-        if (Boundary.knots[1] >= min(knots)) Boundary.knots <- Boundary.knots[-1]
-        kx <- sort(c(knots, Boundary.knots))
+    if (is.logical(Boundary.knots)) {
+        if (Boundary.knots) Boundary.knots <- range(x, na.rm=TRUE) # ns default
+        else Boundary.knots <- NULL
+    }
+    
+    if (length(Boundary.knots) == 0) kx <- sort(knots)
+    else if (length(Boundary.knots) ==2) {
+        if (length(knots) == 0) kx <- Boundary.knots
+        else {
+            if (Boundary.knots[2] <= max(knots)) 
+                Boundary.knots <- Boundary.knots[1]
+            if (Boundary.knots[1] >= min(knots)) 
+                Boundary.knots <- Boundary.knots[-1]
+            kx <- sort(c(knots, Boundary.knots))
         }
+    } else stop("wrong length for Boundary.knots")
 
     j <- c(1, length(kx))
     bknot <- kx[j]
