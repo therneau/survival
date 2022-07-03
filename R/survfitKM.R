d492572c2defc2d1781a987ea91f878b96f6f2b5
Dan Chaltiel <dan.chaltiel@gmail.com>
2022-07-03 14:11:26 +0200

    avoid partial matching
    use $std.err instead of $std

--- R/survfitKM.R
+++ R/survfitKM.R
@@ -211,9 +211,9 @@ survfitKM <- function(x, y, weights=rep(1.0,length(x)),
                      n.event= cfit$n[,5],
                      n.censor=cfit$n[,6],
                      surv = cfit$estimate[,1],
-                     std.err = cfit$std[,1],
+                     std.err = cfit$std.err[,1],
                      cumhaz  = cfit$estimate[,2],
-                     std.chaz = cfit$std[,2])
+                     std.chaz = cfit$std.err[,2])
      } else {
          strata <- sapply(cfit, function(x) if (is.null(x$n)) 0L else nrow(x$n))
          names(strata) <- xlev
@@ -224,9 +224,9 @@ survfitKM <- function(x, y, weights=rep(1.0,length(x)),
                       n.event= unlist(lapply(cfit, function(x) x$n[,5])),
                       n.censor=unlist(lapply(cfit, function(x) x$n[,6])),
                       surv =   unlist(lapply(cfit, function(x) x$estimate[,1])),
-                      std.err =unlist(lapply(cfit, function(x) x$std[,1])),
+                      std.err =unlist(lapply(cfit, function(x) x$std.err[,1])),
                       cumhaz  =unlist(lapply(cfit, function(x) x$estimate[,2])),
-                      std.chaz=unlist(lapply(cfit, function(x) x$std[,2])),
+                      std.chaz=unlist(lapply(cfit, function(x) x$std.err[,2])),
                       strata=strata)
           if (ny==3) rval$n.enter <- unlist(lapply(cfit, function(x) x$n[,8]))
     }
